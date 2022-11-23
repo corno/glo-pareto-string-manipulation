ed4b3c3f60b1b8a418a709fd2b2a8af10316d729
corno <corno.astn.a@schraverus.com>
2022-11-23 09:51:52 +0100

    first commit

---
+++ pub/src/interface/functions/functions.p.ts
@@ -0,0 +1,29 @@
+import * as pt from "pareto-core-types"
+
+export type FSplitString = ($: {
+    value: string
+    splitter: string
+}) => pt.Array<string>
+
+export type FSplitStringIn2 = ($: {
+    value: string
+    splitter: string
+}) => pt.Array<{
+    first: string
+    second: string | null
+}>
+
+export type FTrimEnd = ($: string) => string
+
+export type FStringLength = ($: string) => number
+
+export type FSubStr = ($: {
+    readonly "value": string,
+    readonly "begin": number,
+    readonly "length": number,
+}) => string
+
+export type FStartsWith = ($: {
+    readonly "contextString": string
+    readonly "searchString": string
+}) => boolean
